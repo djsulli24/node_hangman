03f2401b98a3a3fea48a2468e019b823d9e042cb
djsulli24 <djsulli24@gmail.com>
2018-01-25 14:13:15 -0600

    Basic constructors/functions are now bug-free.

--- letter.js
+++ letter.js
@@ -1,6 +1,6 @@
 function Letter(letter) {
     this.letter = letter.toUpperCase();
-    this.guessed = ("abcdefghijklmnopqrstuvwxy".indexOf(letter) === -1)? true : false;
+    this.guessed = ("abcdefghijklmnopqrstuvwxyz".indexOf(letter) === -1)? true : false;
 };
 
 module.exports = Letter;
--- node_hangman.js
+++ node_hangman.js
@@ -7,17 +7,31 @@ const wordList = JSON.parse(JSON.stringify(wordListImport));
 function Game(inputWord) {
     this.wordString = inputWord;
     this.word = new wordObject(inputWord);
-}
+};
 
 Game.prototype.renderWord = function() {
     let string = "";
-    for (let char of this.word) {
-        string += char + " ";
+    for (let obj of this.word.playArray) {
+        if (obj.guessed){
+            string += obj.letter + " ";
+        }
+        else {
+            string += "_ ";
+        }
     }
     console.log(string);
-}
+};
+
+Game.prototype.askGuess = function() {
+
+};
+
+Game.prototype.checkGuess = function () {
+
+};
 
 // Creates a new Game object instance with a random word from the wordList.
 var game = new Game(wordList[Math.floor(Math.random()*wordList.length)]);
 
-console.log(game.word)
+console.log(game.wordString);
+game.renderWord();
